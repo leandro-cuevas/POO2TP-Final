7f9de99ad9054b39c47ec684e869849a17b8638d
Leandro <leandroagustin.cuevas@gmail.com>
2023-10-29 18:19:33 -0300

    Implementacion de tramo

--- tp-final/src/ar/edu/unq/po2/TerminalPortuaria/Tramo.java
+++ tp-final/src/ar/edu/unq/po2/TerminalPortuaria/Tramo.java
@@ -1,5 +1,35 @@
 package ar.edu.unq.po2.TerminalPortuaria;
 
+import java.sql.Time;
+
 public class Tramo {
+	
+	TerminalPortuaria origen;
+	
+	TerminalPortuaria destino;
+	
+	private Time tiempoDeDuracion;
+	
+	private double precio;
+
+	public TerminalPortuaria getOrigen() {
+		return origen;
+	}
+
+	public TerminalPortuaria getDestino() {
+		return destino;
+	}
+
+	public Time getTiempoDeDuracion() {
+		return tiempoDeDuracion;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+	
+	
+	
+	
 
 }
