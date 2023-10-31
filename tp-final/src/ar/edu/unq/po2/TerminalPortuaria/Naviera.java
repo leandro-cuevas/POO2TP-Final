6d558eadc14207ca96a02d7826890827f9b20a3d
Ignacio Moreira Toja <ignaciomoreiratoja@outlook.com.ar>
2023-10-31 20:28:01 -0300

    Naviera ampliado y Viaje, faltan correcciones

--- tp-final/src/ar/edu/unq/po2/TerminalPortuaria/Naviera.java
+++ tp-final/src/ar/edu/unq/po2/TerminalPortuaria/Naviera.java
@@ -1,16 +1,37 @@
 package ar.edu.unq.po2.TerminalPortuaria;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import ar.edu.unq.poo2.TerminalGestionada.Circuito;
 
 public class Naviera {
 	
-	private List<Buque> buques = new ArrayList<Buque>();
-	private List<Viaje> viajes = new ArrayList<Viaje>();
-	private List<Circuito> circuitos = new ArrayList<Circuito>();
+	private List<Buque> buques;
+	private List<Viaje> viajes;
+	private List<Circuito> circuitos;
 	
+	public Naviera() {
+		this.buques =  new ArrayList<Buque>();
+		this.viajes = new ArrayList<Viaje>();
+		this.circuitos = new ArrayList<Circuito>();
+	}
+
 	public boolean contienePuertos(TerminalPortuaria origen, TerminalPortuaria destino) {
 		circuitos.stream().anyMatch(circuito->circuito.contienePuertos(origen, destino));
-}
+	}
+	
+	public void agregarBuque(Buque b) {
+		buques.add(b);
+	}
+	
+	public void establecerViaje(LocalDateTime fechaSalida, Circuito circuitoElegido) {
+		Buque buque; //TODO: Falta poder filtrar por buques que no estén en un viaje.
+		viajes.add(new Viaje(fechaSalida, circuitoElegido, buque));
+	}
+	
+	public void agregarCircuito(Circuito c) {
+		circuitos.add(c);
+	}
+	
 }
---
+++ tp-final/src/ar/edu/unq/po2/TerminalPortuaria/Viaje.java
@@ -0,0 +1,40 @@
+package ar.edu.unq.po2.TerminalPortuaria;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+import ar.edu.unq.poo2.TerminalGestionada.Circuito;
+
+public class Viaje {
+
+		private LocalDateTime fechaSalida;
+		private Circuito circuitoRecorrido;
+		private Buque buqueRecorrido;
+		
+		public Viaje(LocalDateTime fechaSalida, Circuito circuitoRecorrido, Buque buqueRecorrido) {
+			this.fechaSalida = fechaSalida;
+			this.circuitoRecorrido = circuitoRecorrido;
+			this.buqueRecorrido = buqueRecorrido;
+		}
+		
+		public LocalDateTime getFechaSalida() {
+			return fechaSalida;
+		}
+		
+		public Circuito getCircuitoRecorrido() {
+			return circuitoRecorrido;
+		}
+		
+		public Buque getBuqueRecorrido() {
+			return buqueRecorrido;
+		}
+		
+		//Calcula la fecha de llegada a un puerto pasado por parámetro
+		public LocalDateTime fechaDeArriboAlPuerto(TerminalPortuaria p) {
+			
+	        int horasViaje = circuitoRecorrido.getTiempoEntrePuertos(circuitoRecorrido.puertoOrigen, p);
+				        
+	        return fechaSalida.plus(horasViaje, ChronoUnit.HOURS);
+		}
+
+}
