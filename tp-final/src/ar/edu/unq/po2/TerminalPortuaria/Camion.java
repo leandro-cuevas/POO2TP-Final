f4085ac59ab079df99b1ef1d9507425f603465fb
Leandro <leandroagustin.cuevas@gmail.com>
2023-11-01 15:40:47 -0300

    Desarrollo parcial de Camion

---
+++ tp-final/src/ar/edu/unq/po2/TerminalPortuaria/Camion.java
@@ -0,0 +1,30 @@
+package ar.edu.unq.po2.TerminalPortuaria;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Camion {
+	
+	Conductor conductor;
+	
+	List<Container> containers;
+	
+	Turno turno;
+	
+	public void solicitarIngreso(TerminalPortuaria terminal) {
+		//
+	}
+	
+	public LocalDateTime horarioDelTurno() {
+		//Retorna el horario del turno asignado a este camión.
+	}
+	
+	public void descargar() {
+		containers.clear();
+	}
+	
+	public void cargar(Container c) {
+		containers.add(c);
+	}
+
+}
