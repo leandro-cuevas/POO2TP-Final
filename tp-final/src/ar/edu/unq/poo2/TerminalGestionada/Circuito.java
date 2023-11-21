5645065780d361bfb971c0d3e176de3e868b806a
Leandro <leandroagustin.cuevas@gmail.com>
2023-11-21 14:59:17 -0300

    Corregido error en el criterio menor distancia

--- tp-final/src/ar/edu/unq/poo2/TerminalGestionada/Circuito.java
+++ tp-final/src/ar/edu/unq/poo2/TerminalGestionada/Circuito.java
@@ -175,5 +175,9 @@ public class Circuito {
 		// Retorna el puerto de origen. El circuito debe tener al menos un tramo.
 		return listaDeTramos.get(0).getOrigen();
 	}
+	
+	public int getCantidadDeTerminalesQueRecorre() {
+		return this.listaDeTramos.size();
+	}
 
 }
--- tp-final/src/ar/edu/unq/poo2/TerminalGestionada/CriterioMenorDistancia.java
+++ tp-final/src/ar/edu/unq/poo2/TerminalGestionada/CriterioMenorDistancia.java
@@ -3,6 +3,6 @@ package ar.edu.unq.poo2.TerminalGestionada;
 public class CriterioMenorDistancia extends Criterio {
 	@Override
 	public int comparar(Circuito c1, Circuito c2) {
-		return Double.compare(c1.getTiempo(), c2.getTiempo());
+		return Double.compare(c1.getCantidadDeTerminalesQueRecorre(), c2.getCantidadDeTerminalesQueRecorre());
 	}
 }
